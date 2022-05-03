d6c0d82945d276f45b59d54db00c565d37c16e6f
Alexsandro Euzebio da Silva <alexsandro.euzebio@capoviagens.com.br>
2022-05-03 20:01:46 -0300

    Including accessibility concepts, using Popover of the @headlessui/react.

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,25 +1,21 @@
+import { Popover } from '@headlessui/react';
 import { ChatTeardropDots } from 'phosphor-react';
-import { useState } from 'react';
 
 const Widget = () => {
-    const [isWidgetOpen, setIsWidgetOpen] = useState(false)
 
     return (
-        <div className='absolute bottom-5 right-5'>
-            {isWidgetOpen && <p>Hello Word</p>}
+        <Popover className='absolute bottom-5 right-5'>
+            <Popover.Panel>Hello Word</Popover.Panel>
 
-            <button
-                className='bg-brand-500 rounded-full px-3 h-16 text-white flex items-center group'
-                onClick={() => setIsWidgetOpen(oldState => !oldState)}
-            >
+            <Popover.Button className='bg-brand-500 rounded-full px-3 h-16 text-white flex items-center group'>
                 <ChatTeardropDots className='w-10 h-10' />
 
                 <span className='max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear'>
                     <span className='pl-2'></span>
                     Feedback
                 </span>
-            </button>
-        </div>
+            </Popover.Button>
+        </Popover>
     );
 };
 
